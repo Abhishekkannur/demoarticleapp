be70b4878f0d1f87ea811ab574b4d37a1bdd2077
abhishekkannur <abhishek17rd@gmail.com>
2023-06-19 16:56:00 +0530

    first commit

---
+++ articleapp/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ArticleappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'articleapp'
+    def ready(self):
+        import articleapp.signals
